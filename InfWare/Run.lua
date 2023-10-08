009868b4d54f11f5947ef073434cf0cb5fe8d09a
pj0001 <128475489+PJ-0001@users.noreply.github.com>
2023-10-08 09:52:35 +0200

    Update Run.lua

--- InfWare/Run.lua
+++ InfWare/Run.lua
@@ -1,6 +1,6 @@
 
 if gameId == 13772394625 then
- loadstring(game:HttpGet(('https://raw.githubusercontent.com/PJ-0001/Infinite-Ware/main/InfWare/BladeBallIncluded.lua'), true))()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/PJ-0001/Infinite-Ware/main/InfWare/BladeBallIncluded.lua'), true))()
 else
-  loadstring(game:HttpGet(('https://raw.githubusercontent.com/PJ-0001/Infinite-Ware/main/InfWare/WithoutBladeBall.lua'), true))()
+     loadstring(game:HttpGet(('https://raw.githubusercontent.com/PJ-0001/Infinite-Ware/main/InfWare/WithoutBladeBall.lua'), true))()
 end
